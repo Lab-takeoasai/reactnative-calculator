7bbdd7607b4105bf070c10de0001cfdb76fc3e55
Takeo Asai <spitson+github@gmail.com>
2017-04-10 08:05:12 +0900

    Add Reducer test

---
+++ __tests__/reducers/calc.js
@@ -0,0 +1,9 @@
+import * as calc from '../../src/reducers/calc';
+
+describe('actions', () => {
+  it('should create an action to add', () => {
+
+    // Reducers.default.
+    console.log(calc.default)
+  })
+})
