a056386af230b8668ecaf256438553faf1adbed4
Takeo Asai <spitson+github@gmail.com>
2017-04-11 04:42:16 +0900

    Add exsiting component into storybook

--- storybook/stories/CenterView/index.js
+++ storybook/stories/CenterView/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import style from './style';
 
 export default function CenterView(props) {
   return (
     <View style={style.main}>
+      <Text>CenterView</Text>
       {props.children}
     </View>
   );
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import { Text } from 'react-native';
 import { storiesOf, action, linkTo } from '@kadira/react-native-storybook';
 
+import Reducers from '../../src/reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(Reducers);
+
 import Button from './Button';
 import CenterView from './CenterView';
 import Welcome from './Welcome';
 
+import CalcOutput from '../../src/components/calc-output';
+import CalcButtons from '../../src/components/calc-buttons';
+
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
     <Welcome showApp={linkTo('Button')}/>
+  ));
+
+storiesOf('Calc', module)
+  .addDecorator(getStory => (
+    <Provider store={store}>
+      {getStory()}
+    </Provider>
+  ))
+  .add('output', () => (
+    <CalcOutput />
+  ))
+  .add('buttons', () => (
+    <CalcButtons />
   ));
 
 storiesOf('Button', module)
